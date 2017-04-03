f8659affb1c5c3a12ea211500464923fb47b7699
Fabian Klötzl <fabian@kloetzl.info>
2017-04-03 15:21:07 +0200

    refactor

--- src/global.h
+++ src/global.h
@@ -9,5 +9,6 @@ extern size_t NUM_GENES;
 extern double PROBABILITY;
 extern std::string OUT_DIR;
 extern size_t SEED;
+extern double THRESHOLD;
 extern std::default_random_engine RNG;
 extern bool VERBOSE;
