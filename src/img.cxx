f18aaaf2c06a38d35542fd2e2f1b9331536a1f41
Fabian Klötzl <fabian@kloetzl.info>
2017-04-14 20:51:07 +0200

    add times to coalescent

--- src/img.cxx
+++ src/img.cxx
@@ -1,6 +1,7 @@
 #include "img.h"
 #include "config.h"
 #include "global.h"
+#include <cassert>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -11,6 +12,7 @@ class tree_node
 	tree_node *left_child = nullptr;
 	tree_node *right_child = nullptr;
 	double up_time = 0.0;
+	double abs_time = 0.0;
 	ssize_t index = -1; //?
 
   public:
@@ -35,17 +37,49 @@ class tree_node
 		right_child->parent = this;
 	}
 
-	bool is_branch() const
+	void set_time(double t)
+	{
+		assert(t > 0);
+		up_time = t;
+	}
+
+	void set_abs_time(double t)
+	{
+		assert(t > 0);
+		abs_time = t;
+	}
+
+	double get_time() const noexcept
+	{
+		return up_time;
+	}
+
+	double get_abs_time() const noexcept
+	{
+		return abs_time;
+	}
+
+	bool has_parent() const noexcept
+	{
+		return parent != nullptr;
+	}
+
+	tree_node &get_parent() noexcept
+	{
+		return *parent;
+	}
+
+	bool is_branch() const noexcept
 	{
 		return left_child != nullptr;
 	}
 
-	bool is_leaf() const
+	bool is_leaf() const noexcept
 	{
 		return left_child == nullptr;
 	}
 
-	ssize_t get_index() const
+	ssize_t get_index() const noexcept
 	{
 		return index;
 	}
@@ -95,10 +129,15 @@ class tree_node
 			if (self.is_branch()) {
 				ret += ")";
 			}
+			if (self.up_time != 0.0) {
+				ret += ":";
+				ret += std::to_string(self.up_time);
+			}
 		};
 
 		traverse(pre, process, post);
 
+		ret += ";";
 		return ret;
 	}
 };
@@ -136,7 +175,11 @@ void img_model::simulate()
 {
 	// generate coalescent
 	auto n = num_genomes;
+
+	// Use a pool for allocating the tree nodes. Nodes 0 to n are leafes, the
+	// rest are internal nodes, with the last being the root.
 	auto pool = std::vector<tree_node>(2 * n - 1);
+	auto &root = *(pool.end() - 1);
 
 	for (size_t i = 0; i < n; i++) {
 		pool[i] = tree_node::leaf(i);
@@ -153,22 +196,43 @@ void img_model::simulate()
 		return rng_help(RNG);
 	};
 
-	std::cerr << "n: " << n << std::endl;
+	// simulate topology
 	for (size_t i = 0; i < n - 1; i++) {
 		auto p = n + i;
-		std::cerr << "i: " << i << " p: " << p << std::endl;
 		auto c = rand_int(0, n - i);
-		std::cerr << "c: " << c << std::endl;
 		indirect.at(p)->set_left(indirect[c]); // also sets [c].parent
 		indirect[c] = indirect[n - i - 1];
 
 		auto d = rand_int(0, n - i - 1);
-		std::cerr << "d: " << d << std::endl;
 		indirect[p]->set_right(indirect[d]);
 		indirect[d] = indirect[p];
 	}
 
-	std::cerr << indirect[0]->to_newick() << std::endl;
+	// add times
+	auto t = 0.0;
+	auto rand_exp = [](double arg) {
+		auto rng_dist = std::exponential_distribution<double>(arg);
+		return rng_dist(RNG);
+	};
+
+	// simulate absolute times
+	auto sample_size = n;
+	for (size_t i = 0; i < n - 1; i++, sample_size--) {
+		auto ss1 = sample_size * (sample_size - 1) / 2; // N_e ??
+		t += rand_exp(ss1);
+		pool[n + i].set_abs_time(t);
+	}
+
+	// compute relative times
+	root.traverse([](tree_node &self) {
+		if (self.has_parent()) {
+			auto branch_length =
+				self.get_parent().get_abs_time() - self.get_abs_time();
+			self.set_time(branch_length);
+		}
+	});
+
+	std::cout << indirect[0]->to_newick() << std::endl;
 
 	// create core sequences
 	// generate pan genome and create sequences
