9c628dd97df13233fdc10c42b3978600a0b61172
Fabian Klötzl <fabian@kloetzl.info>
2017-08-06 16:32:24 +0200

    update safeguard script

--- scripts/safeguard.zsh
+++ scripts/safeguard.zsh
@@ -1,10 +1,17 @@
 #!/usr/bin/zsh
 
+PM_DIR="~/Projects/3rd/panicmage-master"
+PGS_DIR="$PWD/.."
+
+cd PGS_DIR
+
 integer i
 ITERATIONS=100
-for (( i = 0; i < $ITERATIONS; i++ )); do
-	./pangenomesim -o safeguard --model IMG --param loci_length=100 --param num_genomes=10,img_core_size=200 -p img_theta=200,img_rho=0.2 -p seed=$i 2> /dev/null
-	cd ~/Projects/3rd/panicmage-master
-	./panicmage ../../pangenomesim/safeguard/coalescent.newick ../../pangenomesim/safeguard/genefrequency.gfs 10 2> /dev/null
-	cd -
-done | grep 'p-value' | tee safeguard.txt | awk '/probab/{c++}END{print c/NR}'
+for (( i = 0; i < $ITERATIONS; i++ ));
+do
+	$PGS_DIR/pangenomesim -o $PGS_DIR/safeguard --param gene-length=100 --param num-genomes=10,core-size=200 -p theta=200,rho=0.2 -p seed=$i 2> /dev/null
+	$PM_DIR/panicmage $PGS_DIR/safeguard/coalescent.newick $PGS_DIR/safeguard/genefrequency.gfs 10 2> /dev/null
+done |
+	grep 'p-value' |
+	tee safeguard.txt |
+	awk '/probab/{c++}END{print c/NR}'
